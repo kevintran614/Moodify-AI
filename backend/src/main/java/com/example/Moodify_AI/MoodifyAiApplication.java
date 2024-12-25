34316608500b3254bb27027f856d7f484888e7a1
Kevin Tran <kevinat1@uci.edu>
2024-12-25 00:09:20 -0800

    added backend dir

---
+++ backend/src/main/java/com/example/Moodify_AI/MoodifyAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Moodify_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodifyAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodifyAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Moodify_AI/MoodifyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Moodify_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodifyAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
